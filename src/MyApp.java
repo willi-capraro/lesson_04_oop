1ad98397a808a9df6f7955021f0288a123f91800
Wilhelm Capraro <wilhelm.capraro@gmail.com>
2024-02-08 15:26:40 +0100

    created square calc

--- src/MyApp.java
+++ src/MyApp.java
@@ -1,10 +1,11 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        /*ystem.out.println("Hello world");
         Human h = new Human();
         Car c = new Car();
         Car k = new Car();
+
         h.setName("Herbert");
         c.setBrand("Mercedes");
         k.setBrand("Opel AMG");
@@ -12,8 +13,36 @@ public class MyApp {
         c.setConstructionYear(2024);
         h.introduce();
         c.drive();
-        k.drive();
+        k.drive();*/
 
+        Rectangle r1 = new Rectangle();
+        r1.setSideA(5);
+        r1.setSideB(4);
+        int area1 = r1.calcArea();
+        System.out.println("the area of rectangle 1 is: " + area1);
+        int perimeter1 = r1.calcPermimeter();
+        System.out.println("the perimeter of rectangle 1 is: " + perimeter1);
+
+        Rectangle r2 = new Rectangle();
+        r2.setSideA(5);
+        r2.setSideB(10);
+        if (r2.isSquare()) {
+            System.out.println("r2 is a square, my friend!");
+        }
+        else {
+            System.out.println("r2 is no square, my friend!");
+        }
+        int area2 = r2.calcArea();
+        System.out.println("the area of rectangle 2 is: " + area2);
+        int perimeter2 = r2.calcPermimeter();
+        System.out.println("the perimeter of rectangle 2 is: " + perimeter2);
+
+        if (area2 > area1) {
+            System.out.println("r2 is bigger");
+        }
+        else {
+            System.out.println("r1 is bigger or the same size");
+        }
     }
 
 }
---
+++ src/Rectangle.java
@@ -0,0 +1,33 @@
+public class Rectangle {
+
+    private int sideA = 0;
+    private int sideB = 0;
+
+    public Rectangle() {
+        System.out.println("a new rectangle has been created");
+    }
+    public void setSideA (int newSideA ) {
+        this.sideA = newSideA;
+    }
+    public void setSideB ( int newSideB) {
+        this.sideB =  newSideB;
+    }
+
+    public int calcArea() {
+        int area = this.sideA * this.sideB;
+        return area;
+    }
+    public int calcPermimeter() {
+       int perimeter =  ((this.sideB * 2) + (this.sideA * 2));
+       return perimeter;
+    }
+    public boolean isSquare() {
+        if (this.sideB == this.sideA) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+}
